75c00f6cab46610fbd409561cfa6f690864d4d4e
xden78 <venera.opt@gmail.com>
2024-11-07 18:23:06 +0300

    second

--- src/main/java/com/epam/mjc/collections/list/ListSorter.java
+++ src/main/java/com/epam/mjc/collections/list/ListSorter.java
@@ -13,6 +13,7 @@ public class ListSorter {
 
     public static void main(String[] args) {
         String[] c = new String[]{"-5", "-12", "0", "20", "9", "-20", "37"};
+        // ["0", "-5", "9", "-12", "-20", "20", "37"]
         List<String> a = new ArrayList<>(List.of(c));
         sort(a);
     }
@@ -21,8 +22,11 @@ public class ListSorter {
 class ListComparator implements Comparator<String> {
     @Override
     public int compare(String a, String b) {
-        Integer number1 = Integer.parseInt(a);
-        Integer number2 = Integer.parseInt(b);
+        int number1 = Integer.parseInt(a);
+        int number2 = Integer.parseInt(b);
+        if (Math.abs(number1) == Math.abs(number2)) {
+            return number1 - number2;
+        }
         return function(number1) - function(number2);
     }
 
